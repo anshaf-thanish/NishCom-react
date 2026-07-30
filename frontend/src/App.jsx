76d670c4d11e1467013358c0e94bb5accc8cf0a8
anshaf-thanish <anshafthanish@gmail.com>
2026-07-31 00:06:21 +0530

    register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ProductDetail from './pages/ProductDetail';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 
 const App = () => {
@@ -12,6 +15,13 @@ const App = () => {
       <div className="main-content">
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
       <Footer/> 
